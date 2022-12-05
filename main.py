23147e6f3a5276eb6c4c9876fa8b2819f0f6693c
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2022-12-05 13:18:02 +0100

    Class solution, Class daily, Class weekly, part_time

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ def main():
     data.print_nurses()
     data.print_room()
 
+    solution = Solution(Data(5, 6), 1)
+    solution.print_solution()
+
 if __name__ == "__main__":
     main()
--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 import numpy as np
 import random
+import pandas as pd
+from calendar import monthcalendar
 
 class Data:
 
@@ -17,8 +19,8 @@ class Data:
         for i in range(self.number_of_room):
             self.add_room()
 
-    def add_nurse(self):
-        self.nurses.append(Nurse(self.next_id_nurse))
+    def add_nurse(self, part_time = False):
+        self.nurses.append(Nurse(self.next_id_nurse, part_time))
         self.next_id_nurse += 1
 
     def add_room(self):
@@ -27,7 +29,10 @@ class Data:
 
     def print_nurses(self):
         for i in self.nurses:
-            print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours)
+            if not i.part_time:
+                print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours)
+            else:
+                print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours, ", part-time")
 
     def print_room(self):
         for i in self.rooms:
@@ -35,15 +40,68 @@ class Data:
 
 class Nurse:
 
-    def __init__(self, next_id):
+    def __init__(self, next_id, part_time = False):
         self.status = random.randint(1,5)
         self.id = next_id
         self.number_of_hours = 0
 
+        #Part-time job (children, pregnancy, etc.).
+        self.part_time = part_time
+
+class Daily:
+    """Klasa przechowująca dzienny grafik pielęgniarek"""
+    def __init__(self, data, day):
+        self.day = day
+        self.nurses = data.nurses
+        self.rooms = data.rooms
+        self.daily = np.ndarray(shape=(len(self.rooms), 4), dtype = object)
+
+    #losowe rozwiązanie
+    def random_solution(self):
+        for i in range(self.daily.shape[0]):
+            for j in range(self.daily.shape[1]):
+                self.daily[i][j] = random.choice(self.nurses).id
+
+   #wyświetlenie
+    def print_random_day(self):
+        self.random_solution()
+        daily = pd.DataFrame(self.daily,
+                                   columns=["Day shift (1)", "Day shift (2)", "Night shift (1)", "Night shift (2)"],)
+        print("Day ", self.day, ": ", daily)
+
+
+class Weekly:
+    def __init__(self, data):
+        self.nurses = data.nurses
+        self.rooms = data.rooms
+        self.weekly = pd.DataFrame(Daily(data),
+                                   columns = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                                   index = [0])
+
 class Solution:
-    def __init__(self):
-        data = Data(5, 6)
-        solution = np.ndarray(shape=(4*31, data.number_of_rooms), dtype=int)
+    """Klasa generująca rozwiązanie miesięczne"""
+    def __init__(self, data, month):
+        self.month = month
+        # self.weekly = Weekly(data)
+
+        #Działamy na dniach z kalendarza
+        self.solution = np.array(monthcalendar(2022, month), dtype = object)
+
+        #Każdy dzień z kalendarza jest klasą Daily
+        for i in range(self.solution.shape[0]):
+            for j in range(self.solution.shape[1]):
+                if self.solution[i][j] != 0:
+                    self.solution[i][j] = Daily(data, self.solution[i][j])
+
+    def print_solution(self):
+        days = np.array(monthcalendar(2020, 1), dtype = object)
+        solution = pd.DataFrame(days,
+                                  columns=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+        print(solution)
+        for i in range(self.solution.shape[0]):
+            for j in range(self.solution.shape[1]):
+                if self.solution[i][j] != 0:
+                    self.solution[i][j].print_random_day()
 
 
 class Room:
