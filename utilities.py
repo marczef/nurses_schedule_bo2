b180789de88c7505e29f9f0308dd659bc1abae2b
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2022-12-11 15:38:08 +0100

    add schedule for week

--- utilities.py
+++ utilities.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 import numpy as np
 import random
-import pandas as pd
-from calendar import monthcalendar
+from math import inf
+
 
 class Data:
 
     def __init__(self, number_of_nurses_, number_of_rooms_):
-        self.number_of_room = number_of_rooms_
+        self.number_of_rooms = number_of_rooms_
         self.number_of_nurses = number_of_nurses_
         self.next_id_nurse = 0
         self.next_id_room = 0
@@ -16,11 +16,14 @@ class Data:
 
         for i in range(self.number_of_nurses):
             self.add_nurse()
-        for i in range(self.number_of_room):
+        for i in range(self.number_of_rooms):
             self.add_room()
 
-    def add_nurse(self, part_time = False):
-        self.nurses.append(Nurse(self.next_id_nurse, part_time))
+        self.print_nurses()
+        self.print_room()
+
+    def add_nurse(self):
+        self.nurses.append(Nurse(self.next_id_nurse))
         self.next_id_nurse += 1
 
     def add_room(self):
@@ -29,10 +32,7 @@ class Data:
 
     def print_nurses(self):
         for i in self.nurses:
-            if not i.part_time:
-                print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours)
-            else:
-                print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours, ", part-time")
+            print("Nurse: ", i.id, ", status: ", i.status, ", worked hours: ", i.number_of_hours)
 
     def print_room(self):
         for i in self.rooms:
@@ -40,69 +40,76 @@ class Data:
 
 class Nurse:
 
-    def __init__(self, next_id, part_time = False):
+    def __init__(self, next_id):
         self.status = random.randint(1,5)
         self.id = next_id
         self.number_of_hours = 0
 
-        #Part-time job (children, pregnancy, etc.).
-        self.part_time = part_time
-
-class Daily:
-    """Klasa przechowująca dzienny grafik pielęgniarek"""
-    def __init__(self, data, day):
-        self.day = day
-        self.nurses = data.nurses
-        self.rooms = data.rooms
-        self.daily = np.ndarray(shape=(len(self.rooms), 4), dtype = object)
-
-    #losowe rozwiązanie
-    def random_solution(self):
-        for i in range(self.daily.shape[0]):
-            for j in range(self.daily.shape[1]):
-                self.daily[i][j] = random.choice(self.nurses).id
-
-   #wyświetlenie
-    def print_random_day(self):
-        self.random_solution()
-        daily = pd.DataFrame(self.daily,
-                                   columns=["Day shift (1)", "Day shift (2)", "Night shift (1)", "Night shift (2)"],)
-        print("Day ", self.day, ": ", daily)
-
-
-class Weekly:
-    def __init__(self, data):
-        self.nurses = data.nurses
-        self.rooms = data.rooms
-        self.weekly = pd.DataFrame(Daily(data),
-                                   columns = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-                                   index = [0])
-
 class Solution:
-    """Klasa generująca rozwiązanie miesięczne"""
-    def __init__(self, data, month):
-        self.month = month
-        # self.weekly = Weekly(data)
+    def __init__(self, number_of_nurses_, number_of_rooms_):
+        self.number_of_rooms = number_of_rooms_
+        self.number_of_nurses = number_of_nurses_
+        self.data = Data(self.number_of_nurses, self.number_of_rooms)
+        self.solution = np.ndarray(shape=(4*7, self.data.number_of_rooms,3), dtype=float)
+        self.data.nurses.sort(key=lambda x: 0 if (x.status < 3) else 1)
 
-        #Działamy na dniach z kalendarza
-        self.solution = np.array(monthcalendar(2022, month), dtype = object)
+        for i in range(self.solution.shape[1]):
+            for j in range(self.solution.shape[0]):
+                for nr in range(0,2):
+                    self.solution[j][i][nr] = inf
 
-        #Każdy dzień z kalendarza jest klasą Daily
-        for i in range(self.solution.shape[0]):
-            for j in range(self.solution.shape[1]):
-                if self.solution[i][j] != 0:
-                    self.solution[i][j] = Daily(data, self.solution[i][j])
+        self.two_week()
+        self.data.print_nurses()
 
-    def print_solution(self):
-        days = np.array(monthcalendar(2020, 1), dtype = object)
-        solution = pd.DataFrame(days,
-                                  columns=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-        print(solution)
-        for i in range(self.solution.shape[0]):
-            for j in range(self.solution.shape[1]):
-                if self.solution[i][j] != 0:
-                    self.solution[i][j].print_random_day()
 
+    def first_solution(self, shift, before_before=[]):
+        before = []
+        #To jest po to aby nie dodawało nam bez przerwy tych samych pielęgniarek
+        if shift%2 == 0:
+            random.shuffle(self.data.nurses)
+            self.data.nurses.sort(key=lambda x: 0 if (x.status < 3) else 1)
+        for i in [nurse for nurse in self.data.nurses if nurse.id not in before_before]:
+            for j in self.data.rooms:
+                if j.priority == 2 and i.status >= 3 and \
+                        self.solution[shift][j.id][0] == inf and self.solution[shift][j.id][1] == inf:
+                    self.solution[shift][j.id][0] = i.id
+                    i.number_of_hours += 6
+                    before.append(i.id)
+                    break
+                elif j.priority == 2 and i.status >= 3 and self.solution[shift][j.id][0] != inf \
+                        and self.solution[shift][j.id][1] == inf:
+                    self.solution[shift][j.id][1] = i.id
+                    i.number_of_hours += 6
+                    before.append(i.id)
+                    break
+                elif j.priority == 1 and self.solution[shift][j.id][0] == inf:
+                    self.solution[shift][j.id][0] = i.id
+                    self.solution[shift][j.id][1] = -1
+                    i.number_of_hours += 6
+                    before.append(i.id)
+                    break
+        return before
+
+
+    def two_week(self):
+        self.first_solution(0)
+        before_before = self.first_solution(1)
+        for i in range(2, 28, 2):
+            self.first_solution(i, before_before)
+            before_before = self.first_solution(i+1, before_before)
+
+
+    def write_schedule(self):
+        for i in range(self.solution.shape[1]):
+            for j in range(self.solution.shape[0]):
+                print("[", end="")
+                for nr in range(0,2):
+                    if nr == 0:
+                        print(self.solution[j][i][nr]," ", end="")
+                    else:
+                        print(self.solution[j][i][nr], end="")
+                print("]", end="")
+            print("")
 
 class Room:
     def __init__(self, next_id):
